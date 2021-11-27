5cea4f5a7cac49d9f3f7711081bc0f0200ed7b4d
Jesse Conner <jconner90@gmail.com>
2021-11-26 20:43:45 -0500

    Update Editor.tsx

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import FilePane from './FilePane';
 
+interface IProps { }
 
+interface IState{
+}
 class Editor extends Component {
 
 	render() {
 		return (
-			<div style={{marginLeft:'auto', marginRight:'auto', maxWidth:'1400px',overflowX: 'scroll'}}>
+			<div style={{ marginLeft: 'auto', marginRight: 'auto', maxWidth: '1400px', overflowX: 'scroll' }}>
+				<ul className="nav nav-tabs">
+					<li className="nav-item">
+						<a className="nav-link active" aria-current="page" href="#">File1</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">File2</a>
+					</li>
+				</ul>
 				<FilePane />
-				<FilePane/>
+				<FilePane />
 			</div>
 		);
 	}
