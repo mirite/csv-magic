98630cff1d8508a20e44a63cd787e7761cecc8e8
Jesse Conner <jconner90@gmail.com>
2021-12-31 19:30:56 -0500

    Documentation

--- src/types.ts
+++ src/types.ts
@@ -1,33 +1,93 @@
 import BaseModal from 'components/modals/BaseModal';
+
+/**
+ * A single editor window/file.
+ */
 export interface IFile {
+	/**
+	 * The name of the file that was opened.
+	 */
 	fileName: string;
+	/**
+	 * The current state of the table in the file
+	 */
 	table: ITable;
 	/**
 	 * An array of currently active sorting methods.
 	 */
 	activeSorts: ISorts;
+	/**
+	 * A list of previous states of the file.
+	 */
 	history: IFileHistory;
 }
 
+/**
+ * A list of previous versions of the table in a file.
+ */
 export interface IFileHistory extends Array<ITable> {}
 
+/**
+ * A list of sorts being applied to the table, with the key as the first half of
+ * a tuple and ascending? as the second.
+ */
 export interface ISorts extends Array<[string, boolean]> {}
 
+//Table Elements
+
+/**
+ * A table with rows and columns.
+ */
 export interface ITable {
+	/**
+	 * The ID of the fist cell in a table, used as the default cell being edited.
+	 */
 	firstCellId?: string;
+
+	/**
+	 * An array of the rows within the table.
+	 */
 	contents: Array<IRow>;
 }
 
+/**
+ * A single row within the table.
+ */
 export interface IRow {
+	/**
+	 * A unique id for keying the row and allowing quicker access to the cells within.
+	 */
 	id?: string;
+
+	/**
+	 * The original line index of the row from when it was loaded from a file.
+	 */
 	originalIndex: number;
+
+	/**
+	 * An array of the cells that comprise the row.
+	 */
 	contents: Array<ICell>;
 }
 
+/**
+ * A single cell within the table.
+ */
 export interface ICell {
-	key: string;
-	value: string;
+	/**
+	 * A unique identifier in the form {row.id?cellIndex}
+	 */
 	id: string;
+
+	/**
+	 * The key or column that the cell belongs to.
+	 */
+	key: string;
+
+	/**
+	 * The text content of the cell.
+	 */
+	value: string;
 }
 
 /**
