bf89b5d90eb02e6fd6d8cf39638b174c4f818ce7
Jesse Conner <jconner90@gmail.com>
2022-01-15 20:50:22 -0500

    Reordering interface working

--- src/components/modals/ReorderColumns.tsx
+++ src/components/modals/ReorderColumns.tsx
@@ -33,20 +33,28 @@ export default class ReorderColumnsModal extends BaseModal<IProps, IState> {
 		const { columns } = this.state;
 		return (
 			<div className={styles.list}>
-				{columns.map((pair) => (
+				{columns.map((column, index) => (
 					<ColumnPosition
-						key={pair.id}
-						value={pair}
+						key={column.id}
+						value={column}
 						onMove={(distance: number) =>
-							this.handleChange(pair, distance)
+							this.handleChange(index, distance)
 						}
+						toStart={index}
+						toEnd={columns.length - 1 - index}
 					/>
 				))}
 			</div>
 		);
 	}
-	handleChange(pair: IColumn, distance: number) {
-		throw new Error('Method not implemented.');
+	handleChange(initialIndex: number, distance: number) {
+		const { columns } = this.state;
+		const newIndex = initialIndex + distance;
+		const newOrder = [...columns];
+		const columnToMove = newOrder[initialIndex];
+		newOrder.splice(initialIndex, 1);
+		newOrder.splice(newIndex, 0, columnToMove);
+		this.setState({ columns: newOrder });
 	}
 
 	handleApply(): void {
