44d9128efd135f43796cb779a5115df668801e06
Jesse Conner <jconner90@gmail.com>
2022-03-04 20:25:58 -0500

    Cleanup column generators

--- src/modules/column-generator.ts
+++ src/modules/column-generator.ts
@@ -1,11 +1,32 @@
-import {
-	getCellValueByColumnID,
-	getRowWithMatchingValueInColumn,
-} from './access-helpers';
-import { EGeneratorTypes, ICell, IMappedColumn, IRow, ITable } from 'types';
+import GenerateColumnStrategy, {
+	StrategyParameters,
+} from './column-generators/GenerateColumnStrategy';
+import { Pool } from './column-generators/Pool';
+import { Blank } from './column-generators/Blank';
+import { Duplicate } from './column-generators/Duplicate';
+import { Statically } from './column-generators/Statically';
+import { Lookup } from './column-generators/Lookup';
 import { registerColumnInTable } from './csv-loader';
 import { cloneDeep } from './tools';
-import _ from 'lodash';
+import { EGeneratorTypes, ICell, IMappedColumn, ITable } from 'types';
+
+function getStrategy(
+	method: EGeneratorTypes,
+	methodParameters: StrategyParameters
+): GenerateColumnStrategy {
+	switch (method) {
+		case EGeneratorTypes.pool:
+			return new Pool(methodParameters);
+		case EGeneratorTypes.blank:
+			return new Blank(methodParameters);
+		case EGeneratorTypes.lookup:
+			return new Lookup(methodParameters);
+		case EGeneratorTypes.duplicate:
+			return new Duplicate(methodParameters);
+		case EGeneratorTypes.statically:
+			return new Statically(methodParameters);
+	}
+}
 
 /**
  * Adds a new column to a table and fills it with values using the method and parameters provided.
@@ -22,88 +43,18 @@ export function addColumn(
 	method: EGeneratorTypes,
 	methodParameters: string | string[] | IMappedColumn | undefined
 ): ITable {
-	/**
-	 * Adds a cell to the provided row.
-	 *
-	 * @param  row The row that the cell should be added to.
-	 */
-	const addCellToRow = (row: IRow): void => {
-		/**
-		 * The value of the new cell populated by the method provided.
-		 */
-		const cellValue = generateCellValue(row);
+	const newData = cloneDeep(data) as ITable;
+	const strategy = getStrategy(method, methodParameters);
+	const newColumnId = registerColumnInTable(newData, newColumnName);
 
-		/**
-		 * The new cell to add to the row.
-		 */
+	for (const row of newData.contents) {
+		const cellValue = strategy.getValue(row);
 		const newCell: ICell = {
 			id: row.id + '?' + String(row.contents.length),
 			columnID: newColumnId,
 			value: cellValue,
 		};
 		row.contents.push(newCell);
-	};
-
-	/**
-	 * Generator to evenly assign values from the pool of strings provided.
-	 * Starts at a random index and loops from there.
-	 *
-	 * @return The next string in the pool.
-	 */
-	const poolValuesGenerator = function* () {
-		/**
-		 * The list of strings that can be assigned from.
-		 */
-		const poolValues = methodParameters as Array<string>;
-
-		/**
-		 * The current index within the pool values that are being cycled through.
-		 */
-		let poolValueIndex = _.random(0, poolValues.length - 1, false);
-		while (poolValueIndex < poolValues.length) {
-			yield poolValues[poolValueIndex];
-			poolValueIndex++;
-			if (poolValueIndex === poolValues.length) poolValueIndex = 0;
-		}
-		return '';
-	};
-
-	const getMappedValue = (row: IRow) => {
-		const mappedGenerator = methodParameters as IMappedColumn;
-		const { foreignTable, sourceMatchID, foreignMatchID, foreignImportID } =
-			mappedGenerator;
-
-		const localValue = getCellValueByColumnID(row, sourceMatchID);
-
-		const remoteRow = getRowWithMatchingValueInColumn(
-			foreignTable,
-			foreignMatchID,
-			localValue
-		);
-
-		if (remoteRow)
-			return getCellValueByColumnID(remoteRow, foreignImportID);
-		return '';
-	};
-
-	const getLocalValue = (row: IRow) => {
-		const columnID = methodParameters as string;
-		return getCellValueByColumnID(row, columnID);
-	};
-
-	const generateCellValue = (row: IRow): string => {
-		if (method === EGeneratorTypes.blank) return '';
-		if (method === EGeneratorTypes.statically)
-			return methodParameters as string;
-		if (method === EGeneratorTypes.pool) return poolValues.next().value;
-		if (method === EGeneratorTypes.duplicate) return getLocalValue(row);
-		return getMappedValue(row);
-	};
-
-	const newData = cloneDeep(data) as ITable;
-	const newColumnId = registerColumnInTable(newData, newColumnName);
-	const poolValues = poolValuesGenerator();
-
-	newData.contents.forEach(addCellToRow);
+	}
 	return newData;
 }
---
+++ src/modules/column-generators/Lookup.ts
@@ -0,0 +1,26 @@
+import GenerateColumnStrategy from './GenerateColumnStrategy';
+import { IMappedColumn, IRow } from '../../types';
+import {
+	getCellValueByColumnID,
+	getRowWithMatchingValueInColumn,
+} from '../access-helpers';
+
+export class Lookup extends GenerateColumnStrategy {
+	getValue(row: IRow | undefined): string {
+		const mappedGenerator = this.methodParameters as IMappedColumn;
+		const { foreignTable, sourceMatchID, foreignMatchID, foreignImportID } =
+			mappedGenerator;
+
+		const localValue = getCellValueByColumnID(row as IRow, sourceMatchID);
+
+		const remoteRow = getRowWithMatchingValueInColumn(
+			foreignTable,
+			foreignMatchID,
+			localValue
+		);
+
+		if (remoteRow)
+			return getCellValueByColumnID(remoteRow, foreignImportID);
+		return '';
+	}
+}
