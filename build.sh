7a45bbf2409178bcb7097031a879faac638b089f
Jesse Conner <me@jesseconner.ca>
2024-11-10 17:27:57 -0500

    Off site build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+yarn install
+yarn build
+tar -cvf ./deploy.tar --exclude='*.map' --exclude='.yarn' --exclude='e2e' --exclude='playwright' --exclude='playwright-report' --exclude='tests' --exclude='test-results' --exclude='node_modules' ./*
+
+# Verify the tarball
+if tar -tf ./deploy.tar > /dev/null; then
+    echo "Tarball created successfully."
+else
+    echo "Error creating tarball."
+    exit 1
+fi
+
+caprover deploy -t ./deploy.tar
