d2be1553a100fe2ab6846c9d14d55796835a84a2
Jesse Conner <me@jesseconner.ca>
2025-02-16 12:23:35 -0500

    Fix prettier hook config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 /** @filename: lint-staged.config.js */
 export default {
-	"*": ["prettier . --write --cache"],
+	"*": ["prettier --write --cache"],
 	"*.{ts,tsx,json}": ["sh -c 'tsc --noEmit'"],
 	"*.{js,ts,jsx,tsx,cjs,mjs}": ["eslint --fix --cache"],
 };
